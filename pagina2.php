b96bcd61690e5d2ab769ae6c3f22086de428d95b
Fernando Medeiros <fernandogdemedeirosw@gmail.com>
2021-07-04 13:35:08 -0300

    Organização/Verificação e Botões radio
    Botões de radio página 2/ Um pouco de organização na página 1/ Verificação do resultado na página 3/ Também fiz uma pasta "res" de resources pra gente colocar as fotos que precisa :)

---
+++ pagina2.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Quiz </title>
+</head>
+<body>
+    
+<link rel="stylesheet" type="text/css" href="stylesheet2.css" media="screen" />
+    <?php
+
+    $n = $_POST["name"];
+    $c = $_POST["cpf"];
+
+
+    ?>
+
+
+<form action = "pagina3.php" method = "post">
+
+<br>Questão 1
+<br>
+<br>
+<input type = "radio" name = "x1" id = "x" value = "1"/> Alternativa A
+<br>
+<br>
+<input type = "radio" name = "x1" id = "y" value = "2"/> Alternativa B (Correta)
+<br>
+<br>
+<input type = "radio" name = "x1" id = "z" value = "3"/> Alternativa C
+<br>
+<br>
+<input type = "radio" name = "x1" id = "h" value = "4"/> Alternativa D
+
+<br>
+<br>
+<br>
+<br>
+
+
+<br>Questão 2
+<br>
+<br>
+<input type = "radio" name = "x2" id = "x" value = "1"/> Alternativa A
+<br>
+<br>
+<input type = "radio" name = "x2" id = "y"  value = "2"/> Alternativa B
+<br>
+<br>
+<input type = "radio" name = "x2" id = "z" value = "3" /> Alternativa C (Correta)
+<br>
+<br>
+<input type = "radio" name = "x2" id = "h" value = "4" /> Alternativa D
+
+<br>
+<br>
+
+<br>Questão 3
+<br>
+<br>
+<input type = "radio" name = "x3" id = "x" value = "1"/> Alternativa A (Correta)
+<br>
+<br>
+<input type = "radio" name = "x3" id = "y"  value = "2"/> Alternativa B
+<br>
+<br>
+<input type = "radio" name = "x3" id = "z" value = "3" /> Alternativa C
+<br>
+<br>
+<input type = "radio" name = "x3" id = "h" value = "4" /> Alternativa D
+<br>
+<br>
+
+
+<br>Questão 4
+<br>
+<br>
+<input type = "radio" name = "x4" id = "x" value = "1"/> Alternativa A
+<br>
+<br>
+<input type = "radio" name = "x4" id = "y"  value = "2" /> Alternativa B
+<br>
+<br>
+<input type = "radio" name = "x4" id = "z" value = "3" /> Alternativa C
+<br>
+<br>
+<input type = "radio" name = "x4" id = "h" value = "4"/> Alternativa D (Correta)
+<br>
+<br>
+
+<br>Questão 5
+<br>
+<br>
+<input type = "radio" name = "x5" id = "x" value = "1"/> Alternativa A
+<br>
+<br>
+<input type = "radio" name = "x5" id = "y"  value = "2" /> Alternativa B (Correta)
+<br>
+<br>
+<input type = "radio" name = "x5" id = "z" value = "3"/> Alternativa C
+<br>
+<br>
+<input type = "radio" name = "x5" id = "h" value = "4" /> Alternativa D
+<br>
+<br>
+
+<br>Questão 6
+<br>
+<br>
+<input type = "radio" name = "x6" id = "x" value = "1"/> Alternativa A
+<br>
+<br>
+<input type = "radio" name = "x6" id = "y" value = "2" /> Alternativa B (Correta)
+<br>
+<br>
+<input type = "radio" name = "x6" id = "z" value = "3"/> Alternativa C
+<br>
+<br>
+<input type = "radio" name = "x6" id = "h" value = "4" /> Alternativa D
+
+<br>
+<br>
+
+<br>Questão 7
+<br>
+<br>
+<input type = "radio" name = "x7" id = "x" value = "1"/> Alternativa A (Correta)
+<br>
+<br>
+<input type = "radio" name = "x7" id = "y"  value = "2"/> Alternativa B
+<br>
+<br>
+<input type = "radio" name = "x7" id = "z" value = "3" /> Alternativa C
+<br>
+<br>
+<input type = "radio" name = "x7" id = "h" value = "4"/> Alternativa D
+
+
+<br>
+<br>
+<br>Questão 8
+<br>
+<br>
+<input type = "radio" name = "x8" id = "x" value = "1"/> Alternativa A
+<br>
+<br>
+<input type = "radio" name = "x8" id = "y"  value = "2" /> Alternativa B (Correta)
+<br>
+<br>
+<input type = "radio" name = "x8" id = "z" value = "3" /> Alternativa C
+<br>
+<br>
+<input type = "radio" name = "x8" id = "h" value = "4" /> Alternativa D
+<br>
+<br>
+
+<br>Questão 9
+<br>
+<br>
+<input type = "radio" name = "x9" id = "x" value = "1" /> Alternativa A (Correta)
+<br>
+<br>
+<input type = "radio" name = "x9" id = "y" value = "2" /> Alternativa B
+<br>
+<br>
+<input type = "radio" name = "x9" id = "z" value = "3" /> Alternativa C
+<br>
+<br>
+<input type = "radio" name = "x9" id = "h" value = "4" /> Alternativa D
+
+<br>
+<br>
+<br>Questão 10
+<br>
+<br>
+<input type = "radio" name = "x10" id = "x" value = "1"/> Alternativa A
+<br>
+<br>
+<input type = "radio" name = "x10" id = "y"  value = "2" /> Alternativa B (Correta)
+<br>
+<br>
+<input type = "radio" name = "x10" id = "z" value = "3" /> Alternativa C
+<br>
+<br>
+<input type = "radio" name = "x10" id = "h" value = "4" /> Alternativa D
+
+<br>
+<br>
+
+<input type = "submit" value = "Ver Resultado"/>
+
+
+
+
+
+
+</form>
+</body>
+</html>
---
+++ pagina3.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Quiz</title>
+</head>
+
+<body>
+
+   <?php
+
+   $r = 0;
+
+   $x1 = $_POST['x1'];
+   $x2 = $_POST['x2'];
+   $x3 = $_POST['x3'];
+   $x4 = $_POST['x4'];
+   $x5 = $_POST['x5'];
+   $x6 = $_POST['x6'];
+   $x7 = $_POST['x7'];
+   $x8 = $_POST['x8'];
+   $x9 = $_POST['x9'];
+   $x10 = $_POST['x10'];
+
+
+   if($x1 == 2){
+       $r++;
+
+   }
+    if($x2 == 3 ){
+     $r++;
+   }
+   if($x3 == 1){
+    $r++;
+  }
+
+  if($x4 == 4){
+    $r++;
+  }
+
+  if($x5 ==  2){
+    $r++;
+  }
+
+  if($x6 == 2){
+    $r++;
+  }
+
+  if($x7 == 1 ){
+    $r++;
+  }
+
+  if($x8 == 2){
+    $r++;
+  }
+
+  if($x9 == 1 ){
+    $r++;
+  }
+
+  if($x10 == 2 ){
+    $r++;
+  }
+
+
+if($r >= 7){
+
+    echo "Parabéns, seu desempenho foi de $r acertos, muito bem!";
+}
+
+ if($r < 7){
+
+    echo "Infelizmente sua nota foi de $r acertos, mais sorte na próxima vez.";
+ }
+   ?>
+</body>
+</html>
